0b4567d3d6b0ee92b52576052e305de426b7cdf6
Raviraj96-cyber <babarraviraj62@gmail.com>
2026-04-04 17:54:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // 🔑  PASTE YOUR VAPID KEY HERE  (notifications won't work without it)
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-const FCM_VAPID_KEY = 'BJTbJNtzb3hoiWGpZgyX5sUwgCs7U6qhu6UItw2o0G-uVf22u7xUN96TXRNDMOsh5C8XTJwonBcMNZPZlhOO5ek';
+const FCM_VAPID_KEY = '236045385314';
 
 // ── Firebase config ───────────────────────────────────────────────────────
 const FIREBASE_CONFIG = {
